9adf6ff120df61fee90675b160229b73e5b66b92
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-02 10:50:43 +0100

    Imported my values function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
+
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
   return <div className="App"></div>;
 }
 
