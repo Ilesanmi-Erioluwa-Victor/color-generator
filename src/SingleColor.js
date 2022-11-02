7eb7b906c578687777c3152e955da7826055c87d
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-02 11:35:45 +0100

    Generating my inline bg color

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import rgbToHex from "./util";
 
-const SingleColor = ({ rgb, weight }) => {
-  return <div>SingleColor</div>;
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+
+  // Used for turning array to string seperated comma.
+  const bcg = rgb.join(",");
+  console.log(bcg);
+  return (
+    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+      SingleColor
+    </article>
+  );
 };
 
 export default SingleColor;
